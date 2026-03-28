1109f40b643e151b5f4c110b34cdbf1c22ee0aec
Pablo Dias <pabllosoarez@gmail.com>
2026-03-28 14:01:57 -0300

    Initial Commit

---
+++ Sportcharts/SportchartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportchartsApp.swift
+//  Sportcharts
+//
+//  Created by Pablo Dias on 28/03/26.
+//
+
+import SwiftUI
+
+@main
+struct SportchartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
